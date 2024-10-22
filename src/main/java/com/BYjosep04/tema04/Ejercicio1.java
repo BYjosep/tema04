6b0d590d07f18d542615db1d3f5916b0db374828
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-22 02:14:40 +0200

    Ejercicio1 Terminado

---
+++ src/main/java/com/BYjosep04/tema04/Ejercicio1.java
@@ -0,0 +1,27 @@
+package com.BYjosep04.tema04;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    private static void Sum(float x,float y ){
+    /**
+     * @param x El primer valor.
+     * @param y El segundo valor.
+     * @return La suma de ambos valores.
+
+    */
+        System.out.println((x+y));
+    }
+    public static void main(String[] args) {
+        float primerNumerom, segundoNumero;
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Ingrese el primer numero");
+        primerNumerom = Float.parseFloat(scanner.nextLine());
+        System.out.println("Ingrese el segundo numero");
+        segundoNumero = Float.parseFloat(scanner.nextLine());
+        //Llamada a la funcion Sum
+        Sum(primerNumerom,segundoNumero);
+
+    }
+}
--- src/main/java/com/BYjosep04/tema04/Main.java
+++
@@ -1,7 +0,0 @@
-package com.BYjosep04.tema04;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
