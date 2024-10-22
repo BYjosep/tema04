2ee0b014c96d426c24881a4cf3ececc65d371632
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-22 02:55:10 +0200

    Ejercicio2 Terminado

---
+++ src/main/java/com/BYjosep04/tema04/Ejercicio2.java
@@ -0,0 +1,42 @@
+package com.BYjosep04.tema04;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        float radio;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        radio= Float.parseFloat(scanner.nextLine());
+        scanner.close();
+        System.out.println("El area de un circulo con radio "+radio+" es: "+Area(radio));
+        System.out.println("La circunferencia de un circulo con radio "+radio+" es: "+Circunferencia(radio));
+
+    }
+    // Calculador de area
+    public static double Area(float radio){
+        /**
+          Calcula el área de un círculo.
+          @param radio El radio del círculo.
+         * @return El área calculada del círculo.
+         * @see Math#PI
+         * */
+        if (radio < 0) {
+            System.err.println("El radio no puede ser menor de 0");
+        }
+        return Math.PI * radio * radio;
+    }
+    public static double Circunferencia(float radio){
+        /**
+         * Calcula el área de un círculo.
+         * @param radio El radio del círculo.
+         * @return La circunferencia calculada del círculo.
+         * @see Math#PI
+         * */
+        if (radio < 0) {
+            System.err.println("El radio no puede ser menor de 0");
+        }
+        return Math.PI * (radio *2);
+    }
+
+}
