04f43bac10d2c2d069f0baa7ca97e351369e8ef9
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-23 02:53:40 +0200

    Ejercicio4 Terminado

---
+++ src/main/java/com/BYjosep04/tema04/Ejercicio4.java
@@ -0,0 +1,51 @@
+package com.BYjosep04.tema04;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        float a,b,c,d;
+        int valorMayor;
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Ingrese el primer valor");
+        a= Float.parseFloat(scanner.nextLine());
+        System.out.println("Ingrese el segundo valor");
+        b= Float.parseFloat(scanner.nextLine());
+        System.out.println("Ingrese el tercer valor");
+        c= Float.parseFloat(scanner.nextLine());
+        System.out.println("Ingrese el cuarto valor");
+        d= Float.parseFloat(scanner.nextLine());
+        scanner.close();
+        valorMayor=Mayor(a,b,c,d);
+        switch (valorMayor){
+            case 1-> System.out.printf("El valor mayor es %f que es el 1º valor introducido",a);
+            case 2-> System.out.printf("El valor mayor es %f que es el 2º valor introducido",b);
+            case 3-> System.out.printf("El valor mayor es %f que es el 3º valor introducido",c);
+            case 4-> System.out.printf("El valor mayor es %f que es el 4º valor introducido",d);
+        }
+    }
+    /**
+     * Para poder retransmitrir la salida en un mensaje segun el valor
+     * es favorable usar un switch
+     * @param a Si el primer valor es el mayor es 1
+     * @param b Si el segundo valor es el mayor es 2
+     * @param c Si el tercer valor es el mayor es 3
+     * @param d Si el cuarto valor es el mayor es 4
+     * @return Retorna un valor de 1 al 4
+     */
+    public static int Mayor(float a,float b,float c,float d){
+        int mayor=0;
+        if(a>b && a>c && a>d) {
+            mayor =1;
+        } else if (a<b && b>c && b>d) {
+            mayor=2;
+        }else if (c>a && c>b && c>d) {
+            mayor=3;
+        }else if (d>a && d>b && d>c) {
+            mayor=4;
+        }
+        return mayor;
+
+    }
+
+}
--- src/main/java/com/BYjosep04/tema04/lib/IO.java
+++ src/main/java/com/BYjosep04/tema04/lib/IO.java
@@ -8,40 +8,68 @@ import java.util.Scanner;
 public class IO {
     /**
      * Solicita un messaje y valida si esta comprendido entre longitudMinima y longitudMaxima
-     * @param mensaje Mensaje qye se muestra al usuario
+     *
+     * @param mensaje        Mensaje qye se muestra al usuario
      * @param longitudMinima Numero minimo de caracteres del mensaje
      * @param longitudMaxima Numero maximo de caracteres del mensaje
      * @return El texto leido valido
      */
-    public static String solicitarString(String mensaje, int longitudMinima, int longitudMaxima){
+    public static String solicitarString(String mensaje, int longitudMinima, int longitudMaxima) {
         String mensajeLeido;
-        boolean verificacion =true;
-        Scanner scanner= new Scanner(System.in);
-        do{
+        boolean verificacion = true;
+        Scanner scanner = new Scanner(System.in);
+        do {
             System.out.println(mensaje);
-            mensajeLeido =scanner.nextLine();
-            verificacion = mensajeLeido.length() >= longitudMinima && mensajeLeido.length()<= longitudMaxima;
-            if (!verificacion){
-                System.err.println("La longitud debe de estar entr ["+longitudMinima+"-"+longitudMaxima+"]");
+            mensajeLeido = scanner.nextLine();
+            verificacion = mensajeLeido.length() >= longitudMinima && mensajeLeido.length() <= longitudMaxima;
+            if (!verificacion) {
+                System.err.println("La longitud debe de estar entr [" + longitudMinima + "-" + longitudMaxima + "]");
             }
 
-        }while (!verificacion);
+        } while (!verificacion);
         scanner.close();
         return mensajeLeido;
     }
+
     /**
      * Apartir de dos numeros devuelve un true o false
+     *
      * @param a El primer parametro
      * @param b El segundo parametro
      * @return Retorna el valor true si el primer numero es el mayor y false si es el segundo
      */
-    public static boolean Mayor(float a, float b){
+    public static boolean Mayor(float a, float b) {
         boolean mayor;
-        if (a>b){
-            mayor=true;
-        }else {
-            mayor=false;
+        if (a > b) {
+            mayor = true;
+        } else {
+            mayor = false;
         }
         return mayor;
+    }
+
+    /**
+     * Para poder retransmitrir la salida en un mensaje segun el valor
+     * es favorable usar un switch
+     *
+     * @param a Si el primer valor es el mayor es 1
+     * @param b Si el segundo valor es el mayor es 2
+     * @param c Si el tercer valor es el mayor es 3
+     * @param d Si el cuarto valor es el mayor es 4
+     * @return Retorna un valor de 1 al 4
+     */
+    public static int Mayor(float a, float b, float c, float d) {
+        int mayor = 0;
+        if (a > b && a > c && a > d) {
+            mayor = 1;
+        } else if (a < b && b > c && b > d) {
+            mayor = 2;
+        } else if (c > a && c > b && c > d) {
+            mayor = 3;
+        } else if (d > a && d > b && d > c) {
+            mayor = 4;
+        }
+        return mayor;
+
     }
 }
