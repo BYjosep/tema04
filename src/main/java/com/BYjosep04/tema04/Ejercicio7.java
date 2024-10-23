1ba638a16818b746f464af8e6ec19d0eb5fe6cfd
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2024-10-23 23:27:20 +0200

    Ejercicio7 Terminado

--- src/main/java/com/BYjosep04/tema04/Ejercicio7.java
+++ src/main/java/com/BYjosep04/tema04/Ejercicio7.java
@@ -9,15 +9,18 @@ public class Ejercicio7 {
         System.out.println("Ingrese el numero");
         numero = Integer.parseInt(scanner.nextLine());
         scanner.close();
-        total = factorial(numero);
-        System.out.print(numero + "! = " + numero);
+        total =factorial(numero);
+        if (total<0){
+            System.err.println("El valor representado no es el correctodebido a un overflow");
+        }
+        System.out.println(numero + "! = " + total);
     }
 
     public static long factorial(int numero){
          long factorial=1;
 
         for (int i = numero; i > 1; i--) {
-            factorial *= numero;
+            factorial = factorial* numero;
             numero--;
         }
         return factorial;
